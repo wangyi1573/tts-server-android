e6406109d9d8f271952f0d43810ddac144ea0c0f
jing <42014615+jing332@users.noreply.github.com.>
2024-08-11 15:42:06 +0800

    refactor: New Version System: Catalog

--- app/src/test/java/com/github/jing332/tts_server_android/HttpTtsUrlUnitTest.kt
+++ app/src/test/java/com/github/jing332/tts_server_android/HttpTtsUrlUnitTest.kt
@@ -1,7 +1,6 @@
 package com.github.jing332.tts_server_android
 
 import com.github.jing332.tts_server_android.model.AnalyzeUrl
-import com.github.jing332.tts_server_android.service.systts.help.ResultTextTag
 import org.junit.Test
 
 /**
